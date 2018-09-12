e7680ee105ccddda4cc6db1262f521243a4b3df5
Dotan Dimet <dotan@corky.net>
2018-09-12 19:23:42 +0300

    stuffed to-do example from vue docs in

--- src/components/Stuff.vue
+++ src/components/Stuff.vue
@@ -1,9 +1,63 @@
 <template>
+<div>
 <h2>Stuff</h2>
+  <form v-on:submit.prevent="addNewTodo">
+    <label for="new-todo">Add a todo</label>
+    <input
+      v-model="newTodoText"
+      id="new-todo"
+      placeholder="E.g. Feed the cat"
+    >
+    <button>Add</button>
+  </form>
+  <ul>
+    <li
+      is="Item"
+      v-for="(todo, index) in todos"
+      v-bind:key="todo.id"
+      v-bind:title="todo.title"
+      v-on:remove="todos.splice(index, 1)"
+    ></li>
+  </ul>
+</div>
 </template>
 <script>
+import Item from './Item';
+
 export default {
-    name: 'Stuff'
+    name: 'Stuff',
+    components: {
+        Item
+    },
+    data: function() {
+        return {
+    newTodoText: '',
+    todos: [
+      {
+        id: 1,
+        title: 'Do the dishes',
+      },
+      {
+        id: 2,
+        title: 'Take out the trash',
+      },
+      {
+        id: 3,
+        title: 'Mow the lawn'
+      }
+    ],
+    nextTodoId: 4
+  }
+  },
+  methods: {
+    addNewTodo: function () {
+      this.todos.push({
+        id: this.nextTodoId++,
+        title: this.newTodoText
+      })
+      this.newTodoText = ''
+    }
+  }
 }
 </script>
 

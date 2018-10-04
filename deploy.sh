b0a9df40a87b5499ff21943be1efc8f7e315c381
Dotan Dimet <dotan@corky.net>
2018-10-04 13:44:40 +0300

    enable deploying dist to github pages

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+cd dist
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:dotandimet/masks.git master:gh-pages
+cd -
+

2247dea7dfc53a8a92a5d4a9eefce0bfabf3078a
Dotan Dimet <dotan@corky.net>
2018-10-04 17:15:09 +0300

    add some info to about page and readme

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # masks
 
+This is supposed to eventually be a character sheet replacement for [Masks](http://www.magpiegames.com/masks/), a roleplaying game from Magpie Games.
+
+Source code repository [here](https://github.com/dotandimet/masks/).
+
+Current very non-functional version [here](https://dotandimet.github.io/masks/).
+
 ## Project setup
 ```
 yarn install
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>
+      This is supposed to eventually be a character sheet replacement for <a href="http://www.magpiegames.com/masks/">Masks</a>,
+      a roleplaying game from Magpie Games.
+    </p>
+    <p>Source code repository <a href="https://github.com/dotandimet/masks/">here</a></p>
   </div>
 </template>
